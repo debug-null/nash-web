425cbe8f483a0de2fb53170aef9a4fb544066835
Your Name <you@example.com>
2021-05-03 20:13:43 +0800

    fix remove warning

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,6 +59,8 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
-    // "plugins": ["@babel/plugin-proposal-private-methods", { "loose": true }]
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    }
   }
 }
