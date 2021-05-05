bc64e55113e84dd372043158313d3dcdd8424932
史燕斌 <yanbin.shi@ly.com>
2021-05-05 21:23:10 +0800

    feat: 新增海报页

---
+++ static/js/rem.js
@@ -0,0 +1,2 @@
+
+(function(){function a(){var b=document.documentElement.clientWidth;b=b>1284?1284:b;var c=b/1284*16;document.getElementsByTagName("html")[0].style.fontSize=c+"px"}a();window.onresize=a})();
