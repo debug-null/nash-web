1890313fc581dde2e787f7f6d9284966aeedc63a
史燕斌 <yanbin.shi@ly.com>
2021-05-16 20:12:00 +0800

    web端页面-init

---
+++ static/js/webRem.js
@@ -0,0 +1,2 @@
+
+(function(){function a(){var b=document.documentElement.clientWidth;b=b>1920?1920:b;var c=b/1920*16;document.getElementsByTagName("html")[0].style.fontSize=c+"px"}a();window.onresize=a})();
