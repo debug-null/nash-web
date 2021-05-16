304da6575eb257003597bc3742eb579dbc374828
史燕斌 <yanbin.shi@ly.com>
2021-05-17 00:40:52 +0800

    feat: 打包处理

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const BASE_PATH = '/gw';
+const BASE_PATH = '/';
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
--- plugins/device.js
+++ plugins/device.js
@@ -1,16 +1,16 @@
 if (process.browser) {
   let windowWidth = window.screen.width
-  let { origin, pathname } = window.location
+  let { href, pathname } = window.location
   let mobilePath = 'mobile'
   if (windowWidth <= 750 && !pathname.includes(mobilePath)) {
-    window.location.href = `${origin}/${mobilePath}/${
-      pathname === '/' ? '' : pathname
-    }`
+    let link =  `${href}${mobilePath}`;
+    window.location.href =link
   } else if (windowWidth >= 750 && pathname.includes(mobilePath)) {
-    window.isMobile = false
-    window.location.href = `${origin}/${pathname.replace(
+    window.isMobile = false;
+    let link = `${href}/${pathname.replace(
       '/' + mobilePath + '/',
       ''
     )}`
+    window.location.href = link
   }
 }
